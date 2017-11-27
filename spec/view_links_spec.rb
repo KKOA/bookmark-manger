d1892ca5434d050395a1fb90172fbd57e1049133
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-27 22:14:47 +0000

    Added test for creating new link

--- spec/view_links_spec.rb
+++ spec/view_links_spec.rb
@@ -1,11 +1,21 @@
 feature 'View links' do
-  scenario '' do
+  scenario 'View all bookmarks' do
     Link.create(url: 'http://www.makersacademy.com',title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200 #200 return if success
     witin('ul#links')do
       # search for text with ul with id links
       expect(page).to have_content('Makers Academy')
+    end
+  end
+  scenario 'Create a new bookmark' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.zombo.com/'
+    fill_in 'title', with: 'This is Zombocom'
+    click_button 'Create link'
+    expect(current_path).to eq('/links')
+    within 'ul#links' do
+      expect(page).to have_content('This is Zombocom')
     end
   end
 end
