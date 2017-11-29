dd0f012efc7fb1cb5e7d721474cc169b81f64136
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-29 11:04:16 +0000

    Refactored spec to use web_helper

--- spec/create_links_spec.rb
+++ spec/create_links_spec.rb
@@ -2,22 +2,23 @@ feature 'Create links' do
   scenario 'No links' do
     visit '/links'
     click_link 'New Link'
-    fill_in 'url',   with: 'https://www.facebook.com/'
-    fill_in 'title', with: 'Facebook'
-    click_button 'Create link'
+    url = 'https://www.facebook.com/'
+    title = 'Facebook'
+    fill_new_link(url,title)
     expect(current_path).to eq('/links')
-    within 'ul#links' do
+    within 'li#link2' do
       expect(page).to have_content('Facebook')
     end
   end
   scenario 'Links exist' do
+    Link.create(title: 'Facebook', url:'https://www.facebook.com/')
     visit '/links'
     click_link 'New Link'
-    fill_in 'url',   with: 'http://www.google.com/'
-    fill_in 'title', with: 'Google'
-    click_button 'Create link'
+    url = 'http://www.google.com/'
+    title = 'Google'
+    fill_new_link(url,title)
     expect(current_path).to eq('/links')
-    within 'ul#links' do
+    within 'li#link4' do
       expect(page).to have_content('Google')
     end
   end
