b1a030c9d63487427153dbb6d0d2a22f0a5f2eef
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-11-29 11:05:47 +0000

    Update routes functionality

--- app/app.rb
+++ app/app.rb
@@ -9,13 +9,39 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+
+    link = Link.create(url: params[:url], title: params[:title]) # Create link
+    tag = Tag.first_or_create(name: params[:tags]); # Create a tag for the link
+    link.tags << tag # Adding the tag to the link's DataMapper collection.
+    link.save # Save link to tag
     redirect '/links'
   end
 
   get '/links/new' do
     erb :'links/new'
   end
+
+  # Tags
+
+  get '/tags' do
+    @tags = Tag.all
+    erb :'tags/index'
+  end
+
+  get '/tags/new' do
+    erb :'tags/new'
+  end
+
+  post '/tags' do
+
+    name = params[:name]
+    if(!name.empty?) # stop
+      tag = Tag.new(name: params[:name]);
+      tag.save # Save link to tag
+    end
+    redirect '/tags'
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
